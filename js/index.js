d00f160858225525afec905727e58cf21e18f932
haq <19542414+haq@users.noreply.github.com>
2025-07-11 10:05:00 -0400

    cleanup

--- js/index.js
+++
@@ -1,8 +0,0 @@
-window.onload = () => {
-    document.body.style.transition = "background 500ms";
-};
-
-document.querySelector("#theme-toggle-button")
-    .addEventListener("click", () => {
-        document.body.classList.toggle("light-mode");
-    });
